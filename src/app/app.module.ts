c8a9e7d51220123ab0a2b96da7a20019a7852592
Damika <damikaanupama@gmail.com>
2022-12-07 11:53:13 +0530

    Add new repositories, Sign-up, Sign-in, Dashboard, Not-Found, Server-errror

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './view/welcome/welcome.component';
+import { SignInComponent } from './view/sign-in/sign-in.component';
+import { SignUpComponent } from './view/sign-up/sign-up.component';
+import { NotFoundComponent } from './view/not-found/not-found.component';
+import { ServerErrorComponent } from './view/server-error/server-error.component';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    SignInComponent,
+    SignUpComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
