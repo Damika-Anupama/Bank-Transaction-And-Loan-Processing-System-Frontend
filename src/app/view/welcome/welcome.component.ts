8e3857f97e52f9c3af6057ab1ea1e617e6221959
Damika <damikaanupama@gmail.com>
2022-12-08 16:52:25 +0530

    Add content to the sign-up and sign-in pages

--- src/app/view/welcome/welcome.component.ts
+++ src/app/view/welcome/welcome.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
+
 export class WelcomeComponent {
+constructor(private router:Router){}
+gotoSignup() {
+  this.router.navigateByUrl('sign-up');
+}
 
 }
